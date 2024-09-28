4f5551752150b323d278964ba8b32f72a55135b0
niallpcarroll <niallpcarroll@gmail.com>
2024-09-28 16:45:14 +0000

    Remove js alert

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-alert("Recently added: \nAve Verum Corpus (W. Byrd) \nO for a closer walk with God (C.V. Stanford)");
+//alert("Recently added: \nAve Verum Corpus (W. Byrd) \nO for a closer walk with God (C.V. Stanford)");
